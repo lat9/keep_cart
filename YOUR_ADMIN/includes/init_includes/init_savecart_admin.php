c745615814be83d02763cddd3d1f25c31ccfb6b7
torvista <4391026+torvista@users.noreply.github.com>
2022-03-07 19:51:24 +0100

    add max sort order, changed install texts, added admin record

--- YOUR_ADMIN/includes/init_includes/init_savecart_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_savecart_admin.php
@@ -21,25 +21,29 @@ if (!isset($_SESSION['admin_id'])) {
 // now.
 //
 if (!defined('KEEP_CART_ENABLED')) {
+    $result = $db->Execute("SELECT MAX(sort_order) AS sort_order_max FROM " . TABLE_CONFIGURATION . " WHERE configuration_group_id = 15");
+    $max_sort_order = $result->fields['sort_order_max'] + 1;
+
     $db->Execute(
         "INSERT INTO " . TABLE_CONFIGURATION . "
             (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function)
          VALUES
-            ('Keep Visitor\'s Cart?', 'KEEP_CART_ENABLED', 'False', 'Keep a copy of the visitor\'s cart in a cookie.', 15, 50, now(), NULL, 'zen_cfg_select_option(array(\'True\', \'False\'),'),
+            ('\"Keep Cart\": Keep Visitor\'s Cart?', 'KEEP_CART_ENABLED', 'False', 'Store the visitor\'s cart contents in a cookie.', 15, " . ($max_sort_order + 1) . ", now(), NULL, 'zen_cfg_select_option(array(\'True\', \'False\'),'),
 
-            ('Days Before Cart Expires', 'KEEP_CART_DURATION', '30', 'Number of days to keep a visitor\'s cart.', 15, 51,  now(), NULL, NULL),
+            ('\"Keep Cart\": Days Before Cart Expires', 'KEEP_CART_DURATION', '30', 'Number of days to store cookie in visitors browser.', 15, " . ($max_sort_order + 2) . ",  now(), NULL, NULL),
 
-            ('Keep Cart: Secret Key', 'KEEP_CART_SECRET', 'change me', 'Random characters to use as Keep Cart secret key.  <b>Note:</b> The storefront processing is disabled if this value is left as the default <code>change me</code>.', 15, 52, now(), NULL, NULL)"
+            ('\"Keep Cart\": Secret Key', 'KEEP_CART_SECRET', 'change me', 'Random characters to use as a secret key in a second cookie. <b>Note:</b> The storefront processing is disabled if this value is left as the default <code>change me</code>.', 15, "
+        . ($max_sort_order + 3) . ", now(), NULL, NULL)"
     );
-} else {
-    if (version_compare(PHP_VERSION, '7.3.0', '<') && KEEP_CART_ENABLED === 'True') {
-        $db->Execute(
-            "UPDATE " . TABLE_CONFIGURATION . "
+    zen_record_admin_activity('Installed Plugin "Keep Cart"', 'warning');
+
+} elseif (PHP_VERSION_ID < 70300 && KEEP_CART_ENABLED === 'True') {
+    $db->Execute(
+        "UPDATE " . TABLE_CONFIGURATION . "
                 SET configuration_value = 'False',
                     last_modified = now()
               WHERE configuration_key = 'KEEP_CART_ENABLED'
               LIMIT 1"
-        );
-        $messageStack->add("<em>Keep Cart</em> has been disabled; it requires a minimum PHP version of 7.3.0.", 'error');
-    }
+    );
+    $messageStack->add('<em>"Keep Cart"</em> is disabled; it requires PHP minimum version 7.3.0."', 'error');
 }
